21a202461fb4d69ee598584aa5131898336c4513
Tim <hounddog@gmail.com>
2022-05-27 12:16:25 -0400

    Logger working

--- YadaYada.CropperJs/Cropper.razor.cs
+++ YadaYada.CropperJs/Cropper.razor.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Logging;
 
 namespace YadaYada.CropperJs
 {
     public partial class Cropper
     {
+        [Inject] private CropperInterop Interop { get; set; } = null!;
+
+        [Inject] private ILogger<Cropper> Logger { get; set; } = null!;
+
         [Parameter]
-        public string ImageSource { get; set; }
+        public string ImageSource { get; set; } = null!;
+
+        private Task ImageLoaded(ProgressEventArgs arg)
+        {
+            Logger.LogInformation(nameof(ImageLoaded));
+            return Task.CompletedTask;
+        }
     }
 }
---
+++ YadaYada.CropperJs/CropperInterop.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+
+namespace YadaYada.CropperJs;
+
+public class CropperInterop : IAsyncDisposable
+{
+    private readonly Lazy<Task<IJSObjectReference>> _moduleTask;
+
+    public CropperInterop(IJSRuntime jsRuntime)
+    {
+        _moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+            "import", "./_content/YadaYada.CropperJs/cropper.js").AsTask());
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_moduleTask.IsValueCreated)
+        {
+            var module = await _moduleTask.Value;
+            await module.DisposeAsync();
+        }
+    }
+}
