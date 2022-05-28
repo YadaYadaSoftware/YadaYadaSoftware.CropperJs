1dbe00f017c4d946c9546d1fda567611ad480cd1
Tim <hounddog@gmail.com>
2022-05-28 08:29:08 -0400

    builder.Services.AddCropperServices();

--- YadaYada.CropperJs.Example/Program.cs
+++ YadaYada.CropperJs.Example/Program.cs
@@ -10,7 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddSingleton<ExampleJsInterop>();
-builder.Services.AddSingleton<CropperFactory>();
+builder.Services.AddCropperServices();
 //builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddProvider(new ));
 
 await builder.Build().RunAsync();
--- YadaYada.CropperJs/Cropper.razor.cs
+++ YadaYada.CropperJs/Cropper.razor.cs
@@ -6,187 +6,197 @@ using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 
-namespace YadaYada.CropperJs
+namespace YadaYada.CropperJs;
+
+public static class CropperServices
 {
-    public partial class Cropper
+    public static IServiceCollection AddCropperServices(this IServiceCollection services)
     {
-        private ElementReference _image;
+        services.TryAddSingleton<CropperFactory>();
+        return services;
+    }
+}
 
-        [Inject] private ILogger<Cropper> Logger { get; set; } = null!;
-        [Inject] private CropperFactory CropperFactory { get; set; } = null!;
+public partial class Cropper
+{
+    private ElementReference _image;
 
-        [Parameter]
-        public string ImageSource { get; set; } = null!;
+    [Inject] private ILogger<Cropper> Logger { get; set; } = null!;
+    [Inject] private CropperFactory CropperFactory { get; set; } = null!;
 
-        [Parameter]
-        public decimal ZoomLevel
+    [Parameter]
+    public string ImageSource { get; set; } = null!;
+
+    [Parameter]
+    public decimal ZoomLevel
+    {
+        get => _zoomLevel;
+        set
         {
-            get => _zoomLevel;
-            set
-            {
-                if (_zoomLevel == value) return;
-                _zoomLevel = value;
-                ZoomLevelChanged.InvokeAsync(value);
+            if (_zoomLevel == value) return;
+            _zoomLevel = value;
+            ZoomLevelChanged.InvokeAsync(value);
                 
-            }
         }
+    }
 
-        [Parameter]
-        public EventCallback<decimal> ZoomLevelChanged { get; set; }
+    [Parameter]
+    public EventCallback<decimal> ZoomLevelChanged { get; set; }
 
-        private CropperInstance _cropperInstance = null!;
-        private decimal _zoomLevel;
+    private CropperInstance _cropperInstance = null!;
+    private decimal _zoomLevel;
 
-        private async Task ImageLoaded(ProgressEventArgs arg)
+    private async Task ImageLoaded(ProgressEventArgs arg)
+    {
+        Logger.LogInformation(nameof(ImageLoaded));
+        if (_cropperInstance == null)
         {
-            Logger.LogInformation(nameof(ImageLoaded));
-            if (_cropperInstance == null)
-            {
-                var options = new Options();
-                options.OnCropEnd = CropEndHandler;
-                options.OnCrop = CropHandler;
-                options.OnZoom = ZoomHandler;
-                _cropperInstance = await CropperFactory.CreateCropperAsync(_image, options);
-            }
+            var options = new Options();
+            options.OnCropEnd = CropEndHandler;
+            options.OnCrop = CropHandler;
+            options.OnZoom = ZoomHandler;
+            _cropperInstance = await CropperFactory.CreateCropperAsync(_image, options);
         }
+    }
 
-        private void ZoomHandler(decimal ratio)
+    private void ZoomHandler(decimal ratio)
+    {
+        this.ZoomLevel = ratio;
+    }
+
+    #region CropX
+    private int _cropX;
+
+    [Parameter]
+    public int CropX
+    {
+        get => _cropX;
+        set
         {
-            this.ZoomLevel = ratio;
+            if (_cropX == value) return;
+            _cropX = value;
+            CropXChanged.InvokeAsync(value);
         }
+    }
 
-        #region CropX
-        private int _cropX;
+    [Parameter]
+    public EventCallback<int> CropXChanged { get; set; }
 
-        [Parameter]
-        public int CropX
+    #endregion
+
+    #region CropY
+    private int _cropY;
+
+    [Parameter]
+    public int CropY
+    {
+        get => _cropY;
+        set
         {
-            get => _cropX;
-            set
-            {
-                if (_cropX == value) return;
-                _cropX = value;
-                CropXChanged.InvokeAsync(value);
-            }
+            if (_cropY == value) return;
+            _cropY = value;
+            CropYChanged.InvokeAsync(value);
         }
+    }
 
-        [Parameter]
-        public EventCallback<int> CropXChanged { get; set; }
+    [Parameter]
+    public EventCallback<int> CropYChanged { get; set; }
 
-        #endregion
+    #endregion
 
-        #region CropY
-        private int _cropY;
+    #region CropWidth
+    private int _cropWidth;
 
-        [Parameter]
-        public int CropY
+    [Parameter]
+    public int CropWidth
+    {
+        get => _cropWidth;
+        set
         {
-            get => _cropY;
-            set
-            {
-                if (_cropY == value) return;
-                _cropY = value;
-                CropYChanged.InvokeAsync(value);
-            }
+            if (_cropWidth == value) return;
+            _cropWidth = value;
+            CropWidthChanged.InvokeAsync(value);
         }
+    }
 
-        [Parameter]
-        public EventCallback<int> CropYChanged { get; set; }
+    [Parameter]
+    public EventCallback<int> CropWidthChanged { get; set; }
 
-        #endregion
+    #endregion
 
-        #region CropWidth
-        private int _cropWidth;
+    #region CropHeight
+    private int _cropHeight;
 
-        [Parameter]
-        public int CropWidth
+    [Parameter]
+    public int CropHeight
+    {
+        get => _cropHeight;
+        set
         {
-            get => _cropWidth;
-            set
-            {
-                if (_cropWidth == value) return;
-                _cropWidth = value;
-                CropWidthChanged.InvokeAsync(value);
-            }
+            if (_cropHeight == value) return;
+            _cropHeight = value;
+            CropHeightChanged.InvokeAsync(value);
         }
+    }
 
-        [Parameter]
-        public EventCallback<int> CropWidthChanged { get; set; }
+    [Parameter]
+    public EventCallback<int> CropHeightChanged { get; set; }
 
-        #endregion
+    #endregion
+    private decimal _rotation;
+    private DragModeEnum _dragMode;
 
-        #region CropHeight
-        private int _cropHeight;
-
-        [Parameter]
-        public int CropHeight
+    [Parameter]
+    public decimal Rotation
+    {
+        get => _rotation;
+        set
         {
-            get => _cropHeight;
-            set
-            {
-                if (_cropHeight == value) return;
-                _cropHeight = value;
-                CropHeightChanged.InvokeAsync(value);
-            }
+            if (_rotation == value) return;
+            _rotation = value;
+            RotationChanged.InvokeAsync(value);
         }
+    }
 
-        [Parameter]
-        public EventCallback<int> CropHeightChanged { get; set; }
+    [Parameter]
+    public EventCallback<decimal> RotationChanged { get; set; }
 
-        #endregion
-        private decimal _rotation;
-        private DragModeEnum _dragMode;
+    private void CropHandler(CropEventArgs crop)
+    {
+        Logger.LogInformation(crop.X.ToString() + ',' + crop.Y + ',' + crop.Width + ',' + crop.Height + ',' + crop.Rotation);
+        this.CropX = (int) crop.X;
+        this.CropY = (int) crop.Y;
+        this.CropWidth = (int) crop.Width;
+        this.CropHeight = (int) crop.Height;
+        this.Rotation = crop.Rotation;
+    }
 
-        [Parameter]
-        public decimal Rotation
+    private void CropEndHandler(CropEnd obj)
+    {
+        Logger.LogInformation(nameof(Cropper) + "." + nameof(CropEndHandler));
+    }
+
+    public Task Zoom(decimal i)
+    {
+        this.ZoomLevel = i;
+        return _cropperInstance.Zoom(i);
+    }
+
+    [Parameter] public EventCallback<DragModeEnum> DragModeChanged { get; set; }
+
+    [Parameter]
+    public DragModeEnum DragMode
+    {
+        get => _dragMode;
+        set
         {
-            get => _rotation;
-            set
-            {
-                if (_rotation == value) return;
-                _rotation = value;
-                RotationChanged.InvokeAsync(value);
-            }
-        }
-
-        [Parameter]
-        public EventCallback<decimal> RotationChanged { get; set; }
-
-        private void CropHandler(CropEventArgs crop)
-        {
-            Logger.LogInformation(crop.X.ToString() + ',' + crop.Y + ',' + crop.Width + ',' + crop.Height + ',' + crop.Rotation);
-            this.CropX = (int) crop.X;
-            this.CropY = (int) crop.Y;
-            this.CropWidth = (int) crop.Width;
-            this.CropHeight = (int) crop.Height;
-            this.Rotation = crop.Rotation;
-        }
-
-        private void CropEndHandler(CropEnd obj)
-        {
-            Logger.LogInformation(nameof(Cropper) + "." + nameof(CropEndHandler));
-        }
-
-        public Task Zoom(decimal i)
-        {
-            this.ZoomLevel = i;
-            return _cropperInstance.Zoom(i);
-        }
-
-        [Parameter] public EventCallback<DragModeEnum> DragModeChanged { get; set; }
-
-        [Parameter]
-        public DragModeEnum DragMode
-        {
-            get => _dragMode;
-            set
-            {
-                if(_dragMode==value) return;
-                _dragMode = value;
-                InvokeAsync(async () => { await _cropperInstance.DragMode(value); });
-            }
+            if(_dragMode==value) return;
+            _dragMode = value;
+            InvokeAsync(async () => { await _cropperInstance.DragMode(value); });
         }
     }
 }
