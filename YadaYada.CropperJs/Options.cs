33f57a3919df1de183340aba6cb426fba99c3ea2
Tim <hounddog@gmail.com>
2022-06-01 17:53:02 -0400

    just cleanup

--- YadaYada.CropperJs/Options.cs
+++ YadaYada.CropperJs/Options.cs
@@ -9,24 +9,26 @@ public class Options
     [JsonIgnore]
     public Action<CropEventArgs> OnCrop { get; set; }
 
-    [JSInvokable("crop")]
-    public void Crop(CropEventArgs crop) => OnCrop?.Invoke(crop);
-
     [JsonIgnore]
     public Action<decimal> OnZoom { get; set; }
+
+    [JsonIgnore]
+    public Action OnReady { get; set; } = null!;
+
+    [JSInvokable("ready")]
+    public void Ready() => OnReady.Invoke();
+
+    [JSInvokable("crop")]
+    public void Crop(CropEventArgs crop) => OnCrop?.Invoke(crop);
 
     [JSInvokable("zoom")]
     public void Zoom(decimal ratio) => OnZoom?.Invoke(ratio);
 
-    [JsonIgnore]
-    public Action OnReady { get; set; } = null!;
 
     [JsonPropertyName("dragMode")] public string DragMode { get; set; } = "crop";
     [JsonPropertyName("data")] public CropData Data { get; set; }
     [JsonPropertyName("cropEnabled")] public bool CropEnabled { get; set; }
 
-    [JSInvokable("ready")]
-    public void Ready() => OnReady.Invoke();
 
 
 }
